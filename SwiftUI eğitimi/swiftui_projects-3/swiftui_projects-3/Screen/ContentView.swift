e26b770d276aba7c9c59dd11843b0750a33437a9
Özgür Atak <ozguratak0@gmail.com>
2023-02-02 16:13:39 +0300

    swiftUI - Project3 Update
    - Proje içerisine gesturelar eklendi, bu eklentiler sayesinde proje konusu olan resimlerin büyütülmesi ve sürüklenebilmesi özelliği tamamlandı.
    
    - Clean code olabilmesi için tekrar eden ibareler metodlaştırıldı.

--- SwiftUI eğitimi/swiftui_projects-3/swiftui_projects-3/Screen/ContentView.swift
+++ SwiftUI eğitimi/swiftui_projects-3/swiftui_projects-3/Screen/ContentView.swift
@@ -11,7 +11,15 @@ struct ContentView: View {
     //: MARK: - PROPERTY
     @State private var isAnimating: Bool = false
     @State private var imageScale: CGFloat = 1
+    @State private var imageOffset: CGSize = CGSize(width: 0, height: 0) // burada kısa bir yol olarak CGSize değerini 0,0 olarak vermek yerine direkt .zero atabiliriz
     //: MARK: - FUNCTION
+    
+    func resetImageState() {
+        return withAnimation(.spring()) {
+            imageScale = 1
+            imageOffset = .zero
+        }
+    }
     //: MARK: - CONTENT
     
     
@@ -26,22 +34,40 @@ struct ContentView: View {
                     .padding()
                     .shadow(color: .black.opacity(0.2), radius: 12, x: 2, y: 2)
                     .opacity(isAnimating ? 1 : 0)
+                    .offset(x: imageOffset.width, y: imageOffset.height)
                     .animation(.linear(duration: 1), value: isAnimating)
                     .scaleEffect(imageScale)
-                //MARK: - 1 Tap gesture
+                //MARK: - 1 TAP GESTURE
                     .onTapGesture(count: 2) { //Tap sayacı
                         if imageScale == 1 {
                             withAnimation(.spring()) {
                                 imageScale = 5 //Ölçek büyüten kısım
                             }
                         } else {
-                            withAnimation(.spring()) {
-                                imageScale = 1 // normal değere tekrar dönüş
-                            }
+                            resetImageState() // daha önce imageScale değerini 1 olarak update ediyor ve sıfırlıyorduk fakat uygulama geliştikçe yeni featurelar gereksinimi ile fonksiyon daha clear bir metod oluyor. ayrıca kodun farklı yerlerinde tekrara düşmemek içinde bunu kullanıyoruz. gesture onEnded metodu içinde de aynı gereksinimlere ihtiyacımız var.
+                            
                         }
                     }
+                //MARK: - 2 TAP GESTURE
+                //İmage'ın kullanıcı tarafından sürüklenebilir olmasını sağlamak için eklenen skill
                 
-                //MARK: -
+                    .gesture(
+                        DragGesture()
+                            .onChanged { value in                   // sürükleme hareketi algılandığında
+                                withAnimation(.linear(duration: 1)) {
+                                    imageOffset = value.translation
+                                }
+                            }
+                        
+                            .onEnded({ _ in                         //sürükleme işlemi bırakıldığında
+                                if imageScale <= 1{
+                                    resetImageState()
+                                }
+                            })
+                       
+                    )
+          
+                
             } //: Zstack
             .navigationTitle("Pinch&Zoom")
             .navigationBarTitleDisplayMode(.inline)
